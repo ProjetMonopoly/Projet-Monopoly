f7aa5e04b87f7e790082ad027b9f2b9a32689748
chevillard <Cedric.Chevillard@etu.upmf-grenoble.fr>
2015-05-20 14:52:20 +0200

    Initialisation monopoly classe

---
+++ src/Jeu/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Jeu;
+
+
+
+import Data.MonopolyP;
+import Data.*;
+
+/**
+ *
+ * @author chevillc
+ */
+public class Main {
+    
+    private MonopolyP monop;
+    
+       public static void main(String[] args) {        
+          
+                MonopolyP monop = new MonopolyP("/users/info/pub/1a/M2104/data.txt" );
+                
+       }
+}
+    
